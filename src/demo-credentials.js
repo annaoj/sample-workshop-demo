510a5c37f90fc065902f3e18f6e17eae38e92094
Jimenez <ajimenz@88e9fe519df0.ant.amazon.com>
2023-04-09 17:30:32 -0700

    initial demo

---
+++ src/demo-credentials.js
@@ -0,0 +1,8 @@
+// TODO: Update the value below to match a Cognito Identity Pool created in your
+// AWS account. The unauthenticated IAM role for the pool (usually ending in the
+// suffix "Unauth_Role") must have the following managed permissions policies
+// assigned to it:
+//   - AmazonPollyReadOnlyAccess
+
+export const cognitoIdentityPoolId = 'XXXX'; // 👈 replace this value with your cognito identity pool Id!
+export const sagemakerEndPoint =  'XXXX'; // 👈 replace this value for sagemaker endpoint!
